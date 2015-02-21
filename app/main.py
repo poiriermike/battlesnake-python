31b0ec4841145f174706c6880697697b4fbb0ce2
Robert <drumheadjr@gmail.com>
2015-02-21 14:28:56 -0800

    added list sorting for food and snakes

--- app/main.py
+++ app/main.py
@@ -1,17 +1,24 @@
 import bottle
 import json
+import math
 
 
 def getDistance(item, snakePos):
 
-    pass
+    dx = snakePos[0] - item[0]
+    dy = snakePos[1] - item[1]
 
+    dx = math.fabs(dx)
+    dy = math.fabs(dy)
 
+    return dx + dy
+
+#sorts min-max
 def sortFoodList(unsorted, snakePos):
+    return sorted(unsorted, )
 
-    sorted = sorted(unsorted, key= getDistance(snakePos))
-
-
+def sortSnakeDist(enemeySnakeList, snakePos):
+    return sorted(enemeySnakeList, key=getDistance(snakePos))
 
 
 
@@ -46,6 +53,23 @@ def start():
         'taunt': 'battlesnake-python!'
     })
 
+
+def moveUp():
+    return json.dumps({
+        'move': 'up'
+    })
+def moveDown():
+    return json.dumps({
+        'move': 'down'
+    })
+def moveLeft():
+    return json.dumps({
+        'move': 'left'
+    })
+def moveRight():
+    return json.dumps({
+        'move': 'right'
+    })
 
 @bottle.post('/move')
 def move():
